f387bc27027f655efdde33b880bb2577cfe3fb6e
Liew Jun Young <liewjunyoung@gmail.com>
2023-04-19 22:37:50 +0800

    add theme support

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:track/l10n/l10n.dart';
+import 'package:track/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _AppState();
+}
+
+class _AppState extends State<MyApp> {
+  bool useMaterial3 = true;
+  ThemeMode themeMode = ThemeMode.system;
+  // ColorSeed colorSelected = ColorSeed.baseColor;
+
+  bool get useLightMode {
+    switch (themeMode) {
+      case ThemeMode.system:
+        return SchedulerBinding.instance.window.platformBrightness ==
+            Brightness.light;
+      case ThemeMode.light:
+        return true;
+      case ThemeMode.dark:
+        return false;
+    }
+  }
+
+  void handleBrightnessChange(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      //todo uncomment when complete
+      //debugShowCheckedModeBanner: false,
+      title: 'track',
+      themeMode: themeMode,
+      theme: AppTheme.lightThemeData,
+      darkTheme: AppTheme.darkThemeData,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      //language switching (manual)
-      //locale: Locale.fromSubtags(languageCode: 'es'),
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyPage extends StatefulWidget {
+  const MyPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyPage> createState() => _MyPage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyPage extends State<MyPage> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -75,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-     final l10n = context.l10n;
+    final l10n = context.l10n;
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -105,9 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(AppLocalizations.of(context)!.helloWorld),
-                        Text(l10n.helloWorld),
-
+            Text(l10n.helloWorld),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
